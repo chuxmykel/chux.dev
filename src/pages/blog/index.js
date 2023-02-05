b692b91cf278de933130a9c3059d53d91c9217ee
Chukwudi M. Ngwobia <ngwobiachukwudi@gmail.com>
2023-02-05 15:19:34 +0100

    fix article tags

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -57,7 +57,7 @@ const BlogPage = ({ data }) => {
                   {tags.map((tag) => (
                     <span
                       key={tag}
-                      className="bg-slate-200 dark:bg-slate-700 px-2 rounded font-thin text-base"
+                      className="bg-slate-200 dark:bg-slate-700 px-2 py-1 md:py-0 rounded font-thin text-xs md:text-base"
                     >
                       {tag}
                     </span>
