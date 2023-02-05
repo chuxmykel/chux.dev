abc5adce44ba36cb5d21f12415de8964bbd29563
Chukwudi M. Ngwobia <ngwobiachukwudi@gmail.com>
2023-02-05 15:24:59 +0100

    tag fixes

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -72,7 +72,7 @@ const BlogPost = ({ data, children }) => {
               {tags.map((tag) => (
                 <span
                   key={tag}
-                  className="bg-slate-200 dark:bg-slate-700 px-2 rounded font-thin text-base"
+                  className="bg-slate-200 dark:bg-slate-700 px-2 rounded font-thin text-xs md:text-base"
                 >
                   {tag}
                 </span>
