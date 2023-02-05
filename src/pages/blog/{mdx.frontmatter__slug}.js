5a62006b25412af7afc41127a799087186d73e55
Chukwudi M. Ngwobia <ngwobiachukwudi@gmail.com>
2023-02-05 12:15:48 +0100

    edit blog presentation

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -8,11 +8,21 @@ import { Seo } from "../../components/seo";
 import { CodeBlock } from "../../components/code-block";
 
 const BlogPost = ({ data, children }) => {
+  const {
+    hero_image,
+    title,
+    date,
+    hero_image_alt,
+    hero_image_credit_link,
+    hero_image_credit_text,
+    author,
+    tags,
+  } = data.mdx.frontmatter;
   const components = {
     pre: (props) => <div {...props} className="text-base md:text-lg"></div>,
     code: CodeBlock,
   };
-  const image = getImage(data.mdx.frontmatter.hero_image);
+  const image = getImage(hero_image);
   return (
     <Layout>
       <div className="flex justify-center font-titillium-web w-full min-h-screen">
@@ -21,31 +31,32 @@ const BlogPost = ({ data, children }) => {
             className={`prose dark:prose-invert prose-lg xl:prose-xl prose-slate prose-img:rounded-sm prose-h1:underline prose-headings:underline-offset-8 prose-a:text-blue-600 hover:prose-a:text-blue-400 prose-a:dark:text-blue-400 dark:hover:prose-a:text-blue-300 prose-a:underline prose-a:underline-offset-8 prose-pre:font-source-code-pro`}
           >
             <div className="mb-10">
-              <h1 className="text-4xl md:text-5xl font-extrabold">
-                {data.mdx.frontmatter.title}
-              </h1>
-              <p className="mb-10">
-                Published {data.mdx.frontmatter.date} by{" "}
-                {data.mdx.frontmatter.author}
+              <h1 className="text-4xl md:text-5xl font-extrabold">{title}</h1>
+              <p className="text-base italic">
+                Published: {date} by {author}
               </p>
-              <div className="py-4">
-                <GatsbyImage
-                  image={image}
-                  alt={data.mdx.frontmatter.hero_image_alt}
-                />
+              <GatsbyImage image={image} alt={hero_image_alt} />
+              <div className="flex justify-center">
+                <p className="text-base italic">
+                  Photo Credit:{" "}
+                  <a
+                    href={hero_image_credit_link}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {hero_image_credit_text}
+                  </a>
+                </p>
               </div>
-              <p>
-                Photo Credit:{" "}
-                <a
-                  href={data.mdx.frontmatter.hero_image_credit_link}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {data.mdx.frontmatter.hero_image_credit_text}
-                </a>
-              </p>
             </div>
             <MDXProvider components={components}>{children}</MDXProvider>
+            <div className="mt-20 flex justify-end gap-4">
+              {tags.map((tag) => (
+                <span className="bg-slate-200 dark:bg-slate-700 px-2 rounded font-thin text-base">
+                  {tag}
+                </span>
+              ))}
+            </div>
           </article>
         </div>
       </div>
@@ -61,6 +72,7 @@ export const query = graphql`
         title
         author
         description
+        tags
         date(formatString: "MMM DD, YYYY")
         hero_image_alt
         hero_image_credit_link
@@ -74,11 +86,13 @@ export const query = graphql`
     }
   }
 `;
-export const Head = ({ data }) => (
-  <Seo
-    title={data.mdx.frontmatter.title}
-    description={data.mdx.frontmatter.description}
-  />
-);
+export const Head = ({
+  data: {
+    mdx: {
+      frontmatter: { title, description, tags },
+    },
+  },
+}) => <Seo title={title} description={description} keywords={tags.join(",")} />;
+
 
 export default BlogPost;
