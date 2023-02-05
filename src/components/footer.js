3005282a4c0df859d9c2bd26e861991f9ae3a424
Chukwudi M. Ngwobia <ngwobiachukwudi@gmail.com>
2023-02-05 20:33:41 +0100

    Add privacy policy. (#17)

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import { MdCopyright } from "react-icons/md";
 
 export function Footer() {
   return (
     <>
       <footer className="mt-28 max-sm:text-left text-right pb-4">
-        <span className="text-slate-600 dark:text-slate-400">
-          <MdCopyright className="inline -mt-1" />
-          {` ${new Date().getFullYear()} Copyright. Ngwobia, Chukwudi Mike`}
+        <span className="flex flex-col gap-4 text-slate-600 dark:text-slate-400">
+          <div className="flex justify-end items-center gap-1">
+            <MdCopyright className="inline" />
+            {`${new Date().getFullYear()} Copyright. Ngwobia, Chukwudi Mike`}
+          </div>
+          <div className="flex justify-end items-center gap-1 text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300 text-xs">
+            <a
+              href="https://docs.google.com/document/d/1xiQ_McjCU5--qK6wNuQsJFambfVnM12OgEn9MpJ5fLE/edit?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+              className=""
+            >
+              Privacy Policy
+            </a>
+            <FaExternalLinkAlt />
+          </div>
         </span>
       </footer>
     </>
