59e1be033c32fcb5aa5e64a6130c9873092f8ced
Chukwudi M. Ngwobia <ngwobiachukwudi@gmail.com>
2023-09-18 14:43:07 +0100

    remove adsense from about page (#23)

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,46 +21,6 @@ const AboutPage = () => {
               best practices to create maintainable, testable, and
               production-ready software artifacts.
             </p>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            {/* <p>
-=======
-            <p>
->>>>>>> 5adaf4d (remove adsense from about page (#23))
-=======
-            {/* <p>
->>>>>>> e11178e (update about page)
-              My recent work includes the development of the
-              <a
-                href="https://app.freemancapital.co"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {` Freeman Capital Client Portal `}
-              </a>
-              and this website of course.
-            </p>
-            <blockquote className="py-4 flex items-center">
-              <span className="text-sm bg-slate-200 dark:bg-slate-700 cursor-pointer p-1 text-slate-900 dark:text-slate-50">
-                You can find more of my work on
-                <a
-                  href="https://github.com/chuxmykel"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span>{` .github/ `}</span>
-                </a>
-                😉
-              </span>
-            </blockquote>
-            <p>
-              I am also actively engaged in mentoring and currently, I am
-              working with aspiring software developers at
-              <a href="https://codex.academy/" rel="noreferrer" target="_blank">
-                {` Codex Academy, `}
-              </a>
-              helping them to acquire modern and marketable tech skills.
-            </p> */}
             <p>
               I am an avid music lover and enjoy playing various instruments. In
               my free time, I enjoy skating and swimming.
