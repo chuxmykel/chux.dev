58210862b6f4bbd4955fe584371ac2872660fb0d
Chukwudi Ngwobia <ngwobiachukwudi@gmail.com>
2026-09-23 20:54:12 +0100

    fix(about): compute years of experience

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,11 @@ import Layout from "../components/layout";
 import { Seo } from "../components/seo";
 import { ExternalLink } from "../ui/external-link";
 
+const CAREER_START_YEAR = 2018;
+
 const AboutPage = () => {
+  const yearsOfExperience = new Date().getFullYear() - CAREER_START_YEAR;
+
   return (
     <Layout>
       <div className="font-titillium-web">
@@ -13,9 +17,10 @@ const AboutPage = () => {
           <article className="px-4 prose dark:prose-invert prose-xl xl:prose-2xl prose-slate prose-img:rounded-sm prose-h1:underline prose-headings:underline-offset-8 prose-a:text-blue-600 hover:prose-a:text-blue-400 prose-a:dark:text-blue-400 dark:hover:prose-a:text-blue-300 prose-a:underline prose-a:underline-offset-8">
             <h1>About Me</h1>
             <p>
-              I am a software engineer with over 6 years of professional
-              experience building with fast-paced, cross-cultural, and
-              distributed engineering teams to bring product ideas to life.
+              I am a software engineer with over {yearsOfExperience} years of
+              professional experience building with fast-paced,
+              cross-cultural, and distributed engineering teams to bring
+              product ideas to life.
             </p>
             <p>
               My objective is to deliver value quickly, by applying industry
